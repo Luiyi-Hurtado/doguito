84c21bd3f481cbfb98df359858da2997cb484325
Luiyi-Hurtado <luiyifernando5@gmail.com>
2023-04-13 19:55:37 -0500

    create component 404. this is a page that display a 404error.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './assets/css/base/base.css';
 import './assets/css/components/card.css'
 import About from './pages/About';
 import Home from "./pages/Home"
+import Page404 from './pages/404';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes >
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='*' element={<Page404 />} />
       </Routes >
     </Router >
 
