83c56be6bd0619afa3c4256a5a4e7e0c05615c09
Luiyi-Hurtado <luiyifernando5@gmail.com>
2023-04-13 21:53:27 -0500

    use different hook for create redirection for the pages.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './pages/About';
 import Home from "./pages/Home"
 import Page404 from './pages/404';
 import Header from './assets/components/Header';
+import Post from './pages/Posts';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -15,10 +16,10 @@ function App() {
       <Routes >
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/posts/:id' element={<Post />} />
         <Route path='*' element={<Page404 />} />
       </Routes >
     </Router >
-
   );
 }
 
